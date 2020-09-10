06ade055005bb853b3e873cc69d20c77abb3a3f8
Sam Keathley <samncollette@gmail.com>
2020-09-10 14:43:54 -0700

    added questions and response options

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,23 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders homepage', () => {
+  //shows title, upfront, and button
+  //loads without error
+});
+
+test('start button begins quiz', () => {
+  //button clicks
+  //sends user to first question 
+});
+
+test('selecting answer', () => {
+  //selecting answer greys out unselected options
+  //selecting final answer sends user to results
+})
+
+test('get results', () => {
+  //result loads with no error
+  //result has image and text
+  //clicking button takes user back to start
 });
