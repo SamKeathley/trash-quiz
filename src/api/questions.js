e723dd688f2ae81771d52de2afe9fa722cee5e84
Sam Keathley <samncollette@gmail.com>
2020-09-09 17:21:05 -0700

    began setting up result and question data

---
+++ src/api/questions.js
@@ -0,0 +1,31 @@
+const questions = [
+    {
+        question: "",
+        answers: [
+            {
+                type: "Recycled",
+                content: ""
+            },
+            {
+                type: "Compost",
+                content: ""
+            },
+            {
+                type: "Dumpster Goo",
+                content: ""
+            },
+            {
+                type: "Dumpster Fire",
+                content: ""
+            },
+            {
+                type: "Old Fruit That You Feel Guilty About Not Eating",
+                content: ""
+            },
+            {
+                type: "Car Full Of Fast Food Bags",
+                content: ""
+            }
+        ]
+    },
+]
---
+++ src/api/results.js
@@ -0,0 +1,32 @@
+const resultsMap = [
+    {
+        type: "Recycle",
+        image: "",
+        description: ""
+    },
+    {
+        type: "Compost",
+        image: "",
+        description: ""
+    },
+    {
+        type: "Dumpster Goo",
+        image: "",
+        description: ""
+    },
+    {
+        type: "Dumpster Fire",
+        image: "",
+        description: ""
+    },
+    {
+        type: "Old Fruit That You Feel Guilty About Not Eating",
+        image: "",
+        description: ""
+    },
+    {
+        type: "Car Full Of Fast Food Bags",
+        image: "",
+        description: ""
+    }
+]
